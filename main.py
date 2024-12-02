dfa02361a456ab960fa1b5d5c48de9f34a3bffdd
Haya Mamlouk <hayamamlouk25@gmail.com>
2024-12-02 18:18:35 +0100

    part 1 and 2 done

--- main.py
+++ main.py
@@ -20,6 +20,16 @@ from myData import *
 # Probleme minmaxRegret : Calculer le temps de résolution pour les différentes combinaisons de n et p
 # calcul_tps_resol(minmaxRegret, n_values, p_values, nb_instances)
 
+# Resolution du problème de maxOWA
 # maxOWA(n_projects, n_scenarios, costs, utilities, budget, weights)
-minOWA(n_projects, n_scenarios, costs, utilities, budget, weights)
+
+# Resolution du problème de minOWA
+# minOWA(n_projects, n_scenarios, costs, utilities, budget, weights)
+
+# Probleme maxOWA: Calculer le temps de résolution pour les différentes combinaisons de n et p
+calcul_tps_resol(maxOWA, n_values, p_values, nb_instances, OWA=True)
+
+# Probleme minOWA des regret : Calculer le temps de résolution pour les différentes combinaisons de n et p
+calcul_tps_resol(minOWA, n_values, p_values, nb_instances, OWA=True)
+
 
